256f72b7a90a4a235fe6135e32f58401a5b6f450
Skibidi21-ss <darlast0079@gmail.com>
2026-07-26 21:06:43 +0200

    Update ticketButtons.js

--- src/handlers/ticketButtons.js
+++ src/handlers/ticketButtons.js
@@ -197,7 +197,7 @@ const createTicketModalHandler = {
         interaction.guild,
         interaction.member,
         categoryId,
-        reason
+        reason,
         'none',
         ticketType
       );
